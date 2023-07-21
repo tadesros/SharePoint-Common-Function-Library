3e4bd4f04fed7662c61a8c8abd8dab410852ceb6
Tom <tomadesrosiers@gmail.com>
2023-07-21 15:59:31 -0400

    Update shrpFunctionLibrary.js

--- shrpFunctionLibrary.js
+++ shrpFunctionLibrary.js
@@ -3,22 +3,21 @@
 /**
  * Function: SetDropDown: Set the value of a dropdown given the Strings FieldName  and Value
  **/
-function setDropdown(fieldName, dropDownValue) {
-	fieldName = "Ship From Supplier";
+function setDropdown(fieldName, dropDownValue) {	
 	document.querySelector(`[title="${fieldName}"]`).value = dropDownValue;
 }
 /**
- * Hide a field - Send the name of the Field as String
+ * Hide a field - Send the name of the Field as a String
  * **/
 function hideField(fieldName) {
-	let el = document.querySelector(`[title="${fieldName}"]`).closest("tr");
-	el.style.display = "none";
+	document.querySelector(`[title="${fieldName}"]`).closest("tr").style.display =
+		"none";	
 }
 /**
  * FUNCTION: Lock Field - Send the fieldName
  */
 function lockField(fieldToLock) {
-	 (document.querySelector(
+	let status = (document.querySelector(
 		'[title="' + fieldToLock + '"]'
 	).disabled = true);
 }
@@ -26,8 +25,7 @@ function lockField(fieldToLock) {
  * FUNCTION: Un-Lock Field
  */
 function unlockField(fieldToLock) {
-	 (document.querySelector(
+	let status = (document.querySelector(
 		'[title="' + fieldToLock + '"]'
 	).disabled = false);
 }
-
