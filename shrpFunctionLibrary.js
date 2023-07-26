129f32abd602e81b7f36f2afc3e1dd4fbcfcd093
Tom <tomadesrosiers@gmail.com>
2023-07-26 16:22:45 -0400

    Update shrpFunctionLibrary.js

--- shrpFunctionLibrary.js
+++ shrpFunctionLibrary.js
@@ -48,3 +48,10 @@ function unHideDate(title) {
 		.closest("tr")
 		.show();
 }
+function sleep(milliseconds) {
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+}
